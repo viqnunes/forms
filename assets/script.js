ec3c3fd9a48628d3af2111cd376e4249d1cf9928
Vitória Oliveira <vitoria.oliveira513@gmail.com>
2022-05-30 08:18:15 -0300

    add regex number br + add regex email

--- assets/script.js
+++ assets/script.js
@@ -33,11 +33,29 @@ function validaUF(elemento) {
     });
 }
 
+function validaEmail(elemento) {
+    elemento.addEventListener('focusout', function(event) {
+        event.preventDefault();
+
+        let email = /^[a-z0-9.]+@[a-z0-9.]+\.[a-z]+(\.[a-z]+)?/i;
+        if(this.value.match(email)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "Verifique o preenchimento dos campos em destaque.";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
 function validaCampoNumerico(elemento) {
     elemento.addEventListener('focusout', function(event) {
         event.preventDefault();
 
-        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value;
+        let numero = /(\b\(\d{2}\)\s?[9]?\s?\d{4}(\-|\s)?\d\d{4})|(\b\d{2}\s?[9]?\s?\d{4}(\-|\s)?\d{4})|(\b([9]|[9]\s)?\d{4}(\-|\s)?\d{4})|(\b\d{4}(\-|\s)?\d{4})/im;
 
         if(this.value.match(numero)) {
             document.querySelector('.mensagem').innerHTML = "";
@@ -55,6 +73,11 @@ function validaCampoNumerico(elemento) {
 let campoObrigatorio = document.querySelectorAll('input.obrigatorio');
 for( let emFoco of campoObrigatorio) {
     validaCampo(emFoco);
+}
+
+let campoEmail = document.querySelectorAll('input.email');
+for( let emFoco of campoEmail) {
+    validaEmail(emFoco);
 }
 
 let campoUF = document.querySelectorAll('input.uf');
