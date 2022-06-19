873663351ee9ce87d6b70db716946dc2aa26b130
Vitória Oliveira <vitoria.oliveira513@gmail.com>
2022-06-19 15:30:48 -0300

    add regex cep, input cep

--- assets/script.js
+++ assets/script.js
@@ -33,6 +33,24 @@ function validaUF(elemento) {
     });
 }
 
+function validaCEP(elemento) {
+    elemento.addEventListener('focusout', function(event) {
+        event.preventDefault();
+
+        let cep = /(^[0-9]{5}.[0-9]{3}$)|(^[0-9]{8}$)/;
+        if(this.value.match(cep)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "Verifique o preenchimento dos campos em destaque.";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
 function validaEmail(elemento) {
     elemento.addEventListener('focusout', function(event) {
         event.preventDefault();
@@ -71,21 +89,26 @@ function validaCampoNumerico(elemento) {
 }
 
 let campoObrigatorio = document.querySelectorAll('input.obrigatorio');
-for( let emFoco of campoObrigatorio) {
+for( let emFoco of campoObrigatorio ) {
     validaCampo(emFoco);
 }
 
 let campoEmail = document.querySelectorAll('input.email');
-for( let emFoco of campoEmail) {
+for( let emFoco of campoEmail ) {
     validaEmail(emFoco);
 }
 
+let campoCEP = document.querySelectorAll('input.cep');
+for( let emFoco of campoCEP ) {
+    validaCEP(emFoco);
+}
+
 let campoUF = document.querySelectorAll('input.uf');
-for( let emFoco of campoUF) {
+for( let emFoco of campoUF ) {
     validaUF(emFoco);
 }
 
 let campoNumerico = document.querySelectorAll('input.numero');
-for(let emFoco of campoNumerico) {
+for(let emFoco of campoNumerico ) {
     validaCampoNumerico(emFoco);
 }
